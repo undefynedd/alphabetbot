3cf67b8baf949b5324426d6a11aff29517312de5
Awesomecat713 <64054522+Awesomecat713@users.noreply.github.com>
2021-10-20 22:15:02 -0700

    doesn't kill images anymore

--- bot.py
+++ bot.py
@@ -2,7 +2,14 @@
 # numbers if start with letters
 # 
 
-import discord
+
+import discord          # discord ------
+
+import urllib.request   # youtube ------
+from urllib.parse import parse_qs, urlparse
+import json
+import urllib
+import pprint
 
 letters = {
     'a': ['a', '🅰️', '🇦'],
@@ -47,12 +54,35 @@ class MyClient(discord.Client):
         if len(message.channel.name) == 1:
             good = 0
             
-            for i in letters[message.channel.name]:
+            if len(message.content) == 0:
+                good = 1
             
-                if message.content.lower().strip("*_`~").startswith(i):
-                    good = 1
-                    return
+            if message.content.startswith("https://www.youtube.com/" or "https://youtube.com/" or "https://youtu.be/" or "http://youtu.be"):
+                id = parse_qs(urlparse(message.content).query).get('v')
+                
+                params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % id}
+                url = "https://www.youtube.com/oembed"
+                query_string = urllib.parse.urlencode(params)
+                url = url + "?" + query_string
+
+                with urllib.request.urlopen(url) as response:
+                    response_text = response.read()
+                    data = json.loads(response_text.decode())
+                    print(data)
+                    title = data['title']
                     
+                    for i in letters[message.channel.name]:
+                    
+                        if title.lower().startswith(i):
+                            good = 1
+                            return
+            else:
+                for i in letters[message.channel.name]:
+                
+                    if message.content.lower().strip("*_`~").startswith(i):
+                        good = 1
+                        return
+                        
             if good == 0:
                 await message.delete()
                 await message.channel.send(f"<@{message.author.id}> no", delete_after = 5)
